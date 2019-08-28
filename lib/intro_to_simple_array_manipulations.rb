502030e0becd040c48c3dd6dbc441bb1b13154ab
haider <haider.shah@hotmail.com>
2019-08-28 04:08:52 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -28,8 +28,8 @@ def using_concat(array1,array2)
 array1.concat(array2)
 end
 
-def using_insert(array, element)
-  array.insert[4](element)
+def using_insert(array, elemen)
+  array.insert[4](elemen)
 end
 =begin
 def using_uniq(arary)
